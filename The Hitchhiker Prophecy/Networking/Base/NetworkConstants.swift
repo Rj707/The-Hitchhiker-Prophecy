763350f207a0ff557317db65741f9bb33f3f3cc6
Hafiz Saad <>
2021-06-27 15:59:08 +0500

    Marvel keys for API calling have been added

---
+++ The Hitchhiker Prophecy/Networking/Base/NetworkConstants.swift
@@ -0,0 +1,15 @@
+//
+//  NetworkConstants.swift
+//  The Hitchhiker Prophecy
+//
+//  Created by Mohamed Matloub on 6/10/20.
+//  Copyright © 2020 SWVL. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkConstants {
+    static let baseUrl = "https://gateway.marvel.com"
+    static let publicKey = "aec37e985654b5b7d1b4d2c23e1d3f7d" // Add your marvel keys 
+    static let privateKey = "410da29e5e42f51c560b319e3f12dad3b562557d" // Add your marvel keys
+}
