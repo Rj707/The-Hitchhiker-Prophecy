bc9bccd48a7e5d70de3ea04d4c9d776c950606d3
Hafiz Saad <>
2021-07-01 15:49:08 +0500

    Presenter TODO completed

--- The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
+++ The Hitchhiker Prophecy/Modules/Home/Home Scene/HomeScenePresenter.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 class HomeScenePresneter: HomeScenePresentationLogic {
-    weak var displayView: HomeSceneDisplayView?
+    var displayView: HomeSceneDisplayView?
     
     init(displayView: HomeSceneDisplayView) {
         self.displayView = displayView
@@ -17,6 +17,13 @@ class HomeScenePresneter: HomeScenePresentationLogic {
     
     func presentCharacters(_ response: HomeScene.Search.Response) {
         // TODO: Implement
-        
+        switch response {
+        case .success(let Output):
+            print("")
+            self.displayView?.didFetchCharacters(viewModel: Output.data.toHomeSceneViewMode())
+        case .failure(let error):
+            self.displayView?.failedToFetchCharacters(error: error)
+            
+        }
     }
 }
